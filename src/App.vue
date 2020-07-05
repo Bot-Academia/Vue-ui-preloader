23536f6131ff480939ef86607b1c557a3118eb97
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-07-05 16:32:42 +0530

    setup

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
 
 <!--    loaders-->
     <div v-if="selected=='loader1'">
-      <loader1></loader1>
+      <loader1
+      :wheelcolor="wheelcolor"
+      :bgcolor="bgcolor"
+      :wheelbg="wheelbg"
+      :opacity="opacity"
+      ></loader1>
     </div>
     <div v-else>
       <h1>Select a loader first</h1>
     </div>
       <br><br><br>
+
+
 
 <!--    selectors-->
     <div class="container">
@@ -19,11 +26,26 @@
     </div>
 
       <br><br><br>
+      <!--      props to be passed-->
       <div class="container">
-          <h1>For how long you want to run loader animation?</h1>
+          <h4>Wheel color:</h4>
+          <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="wheelcolor">
+          {{wheelcolor}}
+          <h4>Background Color:</h4>
+          <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="bgcolor">
+          {{bgcolor}}
+          <h4>Background Color opacity:</h4>
+          <b-form-input id="range-1" v-model="opacity" type="range" min="0" max="100"></b-form-input>
+          <div class="mt-2">Opacity-percentage: {{ opacity }}</div>
+          <h4>Wheel Background Color:</h4>
+          <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="wheelbg">
+          {{wheelbg}}
+      </div>
+      <div class="container">
+          <h4>Test Duration:</h4>
           <div>
               <b-form-input id="range-1" v-model="value" type="range" min="0" max="10"></b-form-input>
-              <div class="mt-2">Value: {{ value }}</div>
+              <div class="mt-2">Seconds: {{ value }}</div>
           </div>
       </div>
 
@@ -38,12 +60,16 @@ export default {
   data(){
     return{
       selected: '',
-        value: 5
+        value: 5,
+        wheelcolor: '#ffbb0f',
+        wheelbg: '#999793',
+        opacity: 100,
+        bgcolor: '#343a40'
     };
   },
   name: 'App',
   components: {
-    loader1: loader1
+    loader1: loader1,
   },
   methods: {
     loader1() {
--- src/components/loader1.vue
+++ src/components/loader1.vue
@@ -1,18 +1,35 @@
 <template>
-    <div id="overlay">
-        <div class="loader"></div>
+    <div id="overlay" :style="{'background-color': bgcolors}">
+        <div class="loader" :style="{'border': '5px ' + 'solid ' + wheelbg,'border-top': '5px ' + 'solid ' + wheelcolor}"></div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "static-loader"
+        name: "static-loader",
+        props: ['wheelcolor','wheelbg','opacity','bgcolor'],
+        data() {
+            return{
+                bgcolors: ''
+            };
+        },
+        created() {
+            var c;
+            if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(this.bgcolor)){
+                c= this.bgcolor.substring(1).split('');
+                if(c.length== 3){
+                    c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+                }
+                c= '0x'+c.join('');
+                this.bgcolors= 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',' + this.opacity/100 +')';
+            }
+        }
     }
 </script>
 
 <style scoped>
     #overlay{
-        background-color: rgba(52,58,64,1);
+        /*background-color: rgba(52,58,64,1);*/
         height: 100%;
         width: 100%;
         position: fixed;
@@ -24,8 +41,8 @@
     }
 
     .loader{
-        border: 5px solid grey;
-        border-top: 5px solid orange;
+        /*border: 5px solid grey;*/
+        /*border-top: 5px solid blue;*/
         border-radius: 50%;
         width: 120px;
         height: 120px;
