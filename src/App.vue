1ed39bae6c85a164b0fd17fd3a6aabe42ea93f93
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-07-08 14:38:37 +0530

    ui fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app" class="container">
-
-<!--loaders-->
     <loader :object="styling.objectcolor"
             :color1="styling.animationcolor"
             :color2="styling.animationcolor2"
@@ -12,83 +10,87 @@
             :opacity="styling.opacity"
             :name="selected"></loader>
 
-                <div class="container header">
-                    <h1><b><span id="ui">UI</span><span id="topia">topia</span></b></h1>
-                    <hr class="style-two">
-                    <h1>Vue Pre-Loader</h1>
-                </div>
-<!--formal-->
-      <div class="container">
+      <div class="row justify-content-md-center">
+          <div class="col-md-auto">
+              <a href="https://www.npmjs.com/package/vue-ui-preloader"><img src="https://img.shields.io/npm/v/vue-ui-preloader.svg" class="batch"></a>
+              <a href="https://www.npmjs.com/package/vue-ui-preloader"><img src="https://img.shields.io/npm/dm/vue-ui-preloader.svg" class="batch"></a>
+          </div>
+      </div>
+      <h1><b><span id="ui">UI</span><span id="topia">topia</span></b></h1>
+
+      <p class="row justify-content-md-center">Vue-Ui-Loader is a preloader component made with CSS transistions and animations</p>
+
+      <transition name="fade">
+      <div class="container form" v-if="flag">
           <div class="row">
               <div class="col-sm">
-                  <div class="container main">
-                      <h2>Pre-Loaders</h2>
-                      <div id="outer">
-                          <div class="inner"><button class="btn btn-danger" @click="loader('spinning')" >Spinner</button></div>
-                          <div class="inner"><button class="btn btn-danger" @click="loader('dots')">Dots</button></div>
-                          <div class="inner"><button class="btn btn-danger" @click="loader('loading')">Loading</button></div>
-                          <div class="inner"><button class="btn btn-danger" @click="loader('circular')">Circular</button></div>
-                          <div class="inner"><button class="btn btn-danger" @click="loader('box')">Box</button></div>
-                      </div>
-                  </div>
+                  <h5>Object color:
+                      <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.objectcolor">
+                      {{styling.objectcolor}}</h5>
+
+                  <h5> Additional Color1:
+                      <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.animationcolor">
+                      {{styling.animationcolor}}</h5>
+                  <h5> Additional Color2:
+                      <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.animationcolor2">
+                      {{styling.animationcolor2}}</h5>
+                  <h5>Background color:
+                      <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.bgcolor">
+                      {{styling.bgcolor}}</h5>
+                  <h5>Object bg color:
+                      <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.objectbg">
+                      {{styling.objectbg}}</h5>
               </div>
               <div class="col-sm">
-                  <div class="container test">
-                      <h2>Test Duration:</h2>
-                      <div>
-                          <b-form-input id="range-1" v-model="value" type="range" min="0" max="10"></b-form-input>
-                          <div class="mt-2">Seconds: {{ value }}</div>
-                      </div>
+                  <h5>Background color opacity: {{ styling.opacity }}%</h5>
+                  <b-form-input id="range-1" v-model="styling.opacity" type="range" min="0" max="100"></b-form-input>
+                  <h5>Size of animation: {{ styling.size }}px</h5>
+                  <b-form-input id="range-1" v-model="styling.size" type="range" min="0" max="30"></b-form-input>
+                  <h5>Speed: {{ styling.speed }}s</h5>
+                  <div>
+                      <b-form-input id="range-1" v-model="styling.speed" type="range" min="0.1" max="10" step="0.1"></b-form-input>
                   </div>
               </div>
           </div>
-      </div>
-
-
-
-
-<!--      props to be passed-->
-      <div class="container form">
-          <h1>PlayGround</h1>
-          <h4>Object color:
-          <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.objectcolor">
-          {{styling.objectcolor}}</h4>
-          <h4> Additional color options:</h4>
-          <h5> Color1:
-              <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.animationcolor">
-              {{styling.animationcolor}}</h5>
-          <h5> Color2:
-              <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.animationcolor2">
-              {{styling.animationcolor2}}</h5>
-          <h4>Background color:
-          <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.bgcolor">
-          {{styling.bgcolor}}</h4>
-          <h4>Object bg color:
-              <input type="color" id="favcolor" name="favcolor" value="#ff0000" v-model="styling.objectbg">
-              {{styling.objectbg}}</h4>
-          <h4>Background color opacity: {{ styling.opacity }}%</h4>
-          <b-form-input id="range-1" v-model="styling.opacity" type="range" min="0" max="100"></b-form-input>
-          <h4>Size of animation: {{ styling.size }}px</h4>
-          <b-form-input id="range-1" v-model="styling.size" type="range" min="0" max="30"></b-form-input>
-           <h4>Speed: {{ styling.speed }}s</h4>
+          <hr>
+          <h4>Test Duration: {{ value }}s</h4>
           <div>
-              <b-form-input id="range-1" v-model="styling.speed" type="range" min="0.1" max="10" step="0.1"></b-form-input>
+              <b-form-input id="range-1" v-model="value" type="range" min="0" max="10"></b-form-input>
           </div>
       </div>
+      </transition>
+
+      <transition name="fade">
+      <div class="container" v-if="flag">
+          <div id="outer">
+              <div class="inner"><button class="btn btn-danger" @click="loader('spinning')" ><h2>Spinner</h2></button></div>
+              <div class="inner"><button class="btn btn-danger" @click="loader('dots')"><h2>Dots</h2></button></div>
+              <div class="inner"><button class="btn btn-danger" @click="loader('loading')"><h2>Loading</h2></button></div>
+              <div class="inner"><button class="btn btn-danger" @click="loader('circular')"><h2>Circular</h2></button></div>
+              <div class="inner"><button class="btn btn-danger" @click="loader('box')"><h2>Box</h2></button></div>
+          </div>
+      </div>
+      </transition>
+
+      <div class="container">
+
+      </div>
+<!--      props to be passed-->
+
 
       <br>
 
-   <div class="container"><b-form-textarea
+      <transition name="slide">
+   <div class="container form" v-if="flag">
+       <h1>Playground Code</h1>
+       <b-form-textarea
       id="textarea"
       v-model="code"
       rows="3"
-      placeholder="Your code snippet will be displayed here after you select Pre-Loader"
+      placeholder="Your code snippet will be displayed here after you select Pre-Loader, paste this code after installation."
        ></b-form-textarea></div>
-
+      </transition>
       <br>
-
-
-
   </div>
 </template>
 
@@ -112,6 +114,7 @@ export default {
             speed: 2,
         },
         code: ``,
+        flag: false
     };
   },
   name: 'App',
@@ -126,23 +129,19 @@ export default {
         this.selected='';
       }, this.value*1000);
     }
-  }
+  },
+    mounted() {
+      this.flag=true;
+    }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
-.header{
-  text-align: center;
-  border-radius: 15px !important;
-  box-shadow: 10px 10px 15px -6px rgba(0, 0, 0, 0.75);
-  background-color: rgb(255, 255, 255) !important;
-}
 h1{
     text-align: center;
     padding-bottom: 5px;
     font-family: 'Noto Sans', sans-serif;
-    border-bottom: 1px dashed white;
 }
 .header h1{
   font-family: 'Noto Sans', sans-serif;
@@ -150,30 +149,22 @@ h1{
 }
 #ui{
   color: red;
-  font-size: 50px;
+  font-size: 70px;
   font-family: 'Noto Sans', sans-serif;
 }
-
 #topia{
   color: blue;
+    font-size: 50px;
   font-family: 'Noto Sans', sans-serif;
 }
-
-hr.style-two {
-    border: 0;
-    height: 1px;
-    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
-}
-
-.form,.test,.main{
+.form{
     margin: 10px 10px 10px 10px;
-    padding: 10px 10px 10px 10px;
-    background-color: rgb(22, 51, 105);
+    padding: 20px 20px 20px 20px;
+    background-color: rgb(75, 106, 163);
     border-radius: 15px;
     box-shadow: 10px 10px 15px -6px rgba(0, 0, 0, 0.75);
     color: white;
 }
-
 #textarea{
   text-align: center;
   font-weight: bold;
@@ -183,11 +174,70 @@ hr.style-two {
 {
     width:100%;
     text-align: center;
+    margin: 20px 20px 20px 20px;
 }
 .inner
 {
     margin: 10px 10px 10px 10px;
     display: inline-block;
 }
+.batch{
+    text-align: center;
+    margin: 10px 10px 10px 10px;
+}
+    p{
+        color: lightyellow;
+        font-size: 24px;
+        font-weight: bold;
+    }
+.fade-enter {
+    opacity: 0;
+}
 
+.fade-enter-active {
+    transition: opacity 2s;
+}
+
+.fade-leave-active {
+    transition: opacity 2s;
+    opacity: 0;
+}
+.slide-enter {
+    opacity: 0;
+    /*transform: translateY(20px);*/
+}
+
+.slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity 0.5s;
+}
+
+.slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+    position: absolute;
+}
+
+.slide-move {
+    transition: transform 1s;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(20px);
+    }
+    to {
+        transform: translateY(0);
+    }
+}
+
+@keyframes slide-out {
+    from {
+        transform: translateY(0);
+    }
+    to {
+        transform: translateY(20px);
+    }
+}
 </style>
